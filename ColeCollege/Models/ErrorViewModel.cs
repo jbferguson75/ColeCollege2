30dba3a459357a0e6211d62f1a75f7e3569a0fa9
Josh Ferguson <josh.ferguson@live.com>
2023-11-15 21:05:19 -0700

    Initial 2

---
+++ ColeCollege/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ColeCollege.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ColeCollege/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ColeCollege.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Student Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Student First Name")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Student Enrollment")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
